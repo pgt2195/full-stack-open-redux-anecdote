e125209032def32598dcfcbdd903b46be9841b50
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-08-02 16:41:39 +0200

    resolution du problème de rerender lors du filtre et du tri des anecdotes

--- src/components/DisplayAnecdotes.jsx
+++ src/components/DisplayAnecdotes.jsx
@@ -4,23 +4,27 @@ import { showNotification } from '../reducers/notificationReducer'
 
 const DisplayAnecdotes = () => {
   const dispatch = useDispatch()
+
   
-  const anecdotes = useSelector(({ anecdotes, filter }) => {
-    const anecdotesToDisplay = [...anecdotes].sort((a, b) => b.votes - a.votes) // Tri les anecdotes par nombre de vote
-    // Retourne les anecdotes filtrées avec state.filter
-    return anecdotesToDisplay.filter(anecdote => 
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
+
+  const anecdotesToDisplay = anecdotes
+    .filter(anecdote =>
       anecdote.content.toLowerCase().includes(filter)
     )
-  })
+    .sort((a, b) => b.votes - a.votes)
+
 
   const vote = (anecdote) => {
     dispatch(voteFor(anecdote.id))
     dispatch(showNotification(`Your vote has been registered for: ${anecdote.content}`))
   }
+  
 
   return (
     <div>
-      {anecdotes.map(anecdote =>
+      {anecdotesToDisplay.map(anecdote =>
         <div key={anecdote.id} style={{marginBottom: 10}}>
           <div>
             {anecdote.content}
