f644f5bb53c4dedd83bfa94bc8e2514752008800
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-08-02 11:10:04 +0200

    terminé exercice 6.15

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,16 +1,17 @@
 import { useDispatch } from "react-redux"
 import { createAnecdote } from "../reducers/anecdoteReducer"
 import { showNotification } from "../reducers/notificationReducer"
+import anecdoteService from "../services/anecdotes"
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault() // prevent the refresh of the page
 
-    const userAnecdote = event.target.anecdote.value
-    dispatch(createAnecdote(userAnecdote))
-    dispatch(showNotification(`Your anecdote has been added: ${userAnecdote}`))
+    const newAnecdote = await anecdoteService.createNew(event.target.anecdote.value)
+    dispatch(createAnecdote(newAnecdote))
+    dispatch(showNotification(`Your anecdote has been added: ${newAnecdote.content}`))
     // Clearing the form
     event.target.anecdote.value = ''
   }
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,21 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      return state.concat(asObject(action.payload))
+      return state.concat(action.payload)
     },
     voteFor(state, action) {
       return state.map(anecdote => {
