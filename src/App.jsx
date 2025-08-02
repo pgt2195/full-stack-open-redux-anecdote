ed25f0a9277899f871fb71b92ac55056a516d564
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-08-02 10:24:10 +0200

    terminé exercice 6.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import DisplayAnecdotes from './components/DisplayAnecdotes'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  // Assigne les anecdotes du backend au state correspondant
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [])
+
   return (
     <div>
       <Notification />
